f3452f68789e529e441eeebcb589d13e925ca50e
RicardoSanchez0716 <ricardosanchez0716@gmail.com>
2023-01-30 18:06:00 -0500

    first commit

---
+++ funcion.java
@@ -0,0 +1,39 @@
+public class Main
+{
+		
+	static int resul;
+	public static void suma(int a, int b, int c){
+		
+		int resultado;
+		resultado = a + b + c;
+		
+		System.out.println(resultado);	
+	}
+
+
+    
+    
+    public static void main(String[] args) {
+		
+		suma(50,40,20);
+		
+		Coche miCoche = new Coche();
+
+        resul = miCoche.incrPuertas(miCoche.puertas);
+        System.out.println("El coche ahora cuenta con " + resul + " puertas");
+
+	}
+
+}
+
+ class Coche{
+    
+    int puertas = 4;
+        
+        
+    public static int incrPuertas(int a){
+        a = a + 1;
+        
+        return a;
+    }
+}
