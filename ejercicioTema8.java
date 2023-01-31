fb62aeb7535ad947a9b8e8798a05cdcbff235f06
RicardoSanchez0716 <ricardosanchez0716@gmail.com>
2023-01-31 18:10:25 -0500

    Se agrega ejercicio del tema 8

---
+++ ejercicioTema8.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String[] arg) {
+        
+        Persona persona = new Persona();
+
+        persona.setEdad(25);
+        persona.setNombre("Camilo Martinez");
+        persona.setTelefono("312554545");
+
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+    }
+}
+
+class Persona {
+
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getTelefono(){
+        return telefono;
+    }
+}
