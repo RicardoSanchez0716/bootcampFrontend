96257e7753553ec68f2d6b209bafc9fb2a27e917
RicardoSanchez0716 <ricardosanchez0716@gmail.com>
2023-02-13 16:49:38 -0500

    Se agrega ejercicio 2 del modulo de JavaScript

---
+++ JavaScript/Ejercicio2-js/ejercicio2.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Ricardo",
+    31,
+    true,
+    new date(1991, 7, 16),
+    {
+        titulo: "El señor de los anillos",
+        autor: "J.R.R. Tolkien",
+        fecha: new Date(1954, 7, 29),
+        url: "https://www.buscalibre.com.co/libro-pack-trilogia-el-senor-de-los-anillos-ediciones-con-cartel-de-la-serie-j-r-r-tolkien-booket/9788445013847/p/54216703?bmkt_source=google&bmkt_campaign=878570393&gclid=CjwKCAiA3KefBhByEiwAi2LDHIc2jWxWMp5RhOZ10889gUScjiRzN7W7aAN2JO0TQduKBpIr2ySlGxoC56wQAvD_BwE"
+    }
+]
