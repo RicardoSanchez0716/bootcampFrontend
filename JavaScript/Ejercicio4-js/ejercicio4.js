45ec39d98bf68fe1099c1cf2d43df3270d63217e
RicardoSanchez0716 <ricardosanchez0716@gmail.com>
2023-02-16 16:49:03 -0500

    Se agrega el ejercicio 4 del modulo de Js

---
+++ JavaScript/Ejercicio4-js/ejercicio4.js
@@ -0,0 +1,10 @@
+let nombre = "Ricardo";
+let apellido = "Leon";
+let estudiante = `${nombre} ${apellido}`;
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let numLetras = estudiante.length;
+let primLetra = nombre.charAt();
+let ulrimLetra = apellido[3];
+let sinEspacios = estudiante.trim();
+let contieneNombre = estudiante.includes("Ricardo");
