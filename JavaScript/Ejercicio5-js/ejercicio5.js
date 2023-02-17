e65ab0025e4b59f09dd2ed1876c130ffdf66ad80
RicardoSanchez0716 <ricardosanchez0716@gmail.com>
2023-02-17 15:08:46 -0500

    Se agrega el ejercicio 5 del modulo de js

---
+++ JavaScript/Ejercicio5-js/ejercicio5.js
@@ -0,0 +1,6 @@
+let alturaCm = 178;
+let alturaM = 1.78;
+let peso = 85.2;
+let alturaRedo = Math.ceil(alturaM);
+let pesoRedo = Math.floor(peso);
+let iguales = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
