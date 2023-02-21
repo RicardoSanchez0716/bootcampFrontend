cdb2accb4209ec0992853b5a2cf9cf69654245f4
RicardoSanchez0716 <ricardosanchez0716@gmail.com>
2023-02-20 20:23:43 -0500

    Se agrega ejercicio 7 fechas del modulo de js

---
+++ JavaScript/Ejercicio7-js/fechas.js
@@ -0,0 +1,11 @@
+let hoy =  new Date()
+
+let nacimiento = new Date(1991, 6, 16)
+
+let comparacion = hoy > nacimiento
+
+let diaNacimiento = nacimiento.getDate()
+
+let mesNacimiento = nacimiento.getMonth()
+
+let anioNacimiento = nacimiento.getFullYear()
