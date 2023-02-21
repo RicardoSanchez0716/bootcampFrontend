4db263ee244c80ebb56eddbdd6d957b2afb1de4b
RicardoSanchez0716 <ricardosanchez0716@gmail.com>
2023-02-20 20:07:07 -0500

    Se agrega el ejercicio 7 objetos del modulo de js

--- JavaScript/Ejercicio7-js/conjuntos.js
+++ JavaScript/Ejercicio7-js/conjuntos.js
@@ -1,6 +1,12 @@
-let familia = new Set(["Ricardo", "Ana", "Felipe", "Laura", "Marina", "Julieta", "Elmer"])
+let familia = new Set([
+  "Ricardo",
+  "Ana",
+  "Felipe",
+  "Laura",
+  "Marina",
+  "Julieta",
+  "Elmer",
+]);
 
-familia.add("Ricardo")
-familia.add("Javascript")
-
-
+familia.add("Ricardo");
+familia.add("Javascript");
