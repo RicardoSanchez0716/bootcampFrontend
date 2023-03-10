0969ad1b4fa140e59da5f4224ad405f25fee7f60
RicardoSanchez0716 <ricardosanchez0716@gmail.com>
2023-03-09 22:22:11 -0500

    Se agrega ejercicio 15 del modulo js

---
+++ JavaScript/Ejercicio15-js/index.js
@@ -0,0 +1,13 @@
+let nombre = "Ricardo";
+let apellido = "Leon";
+
+let nombreCompleto = {
+  nombre,
+  apellido,
+}
+
+//sessionStorage.setItem("datos", JSON.stringify(datos))
+//localStorage.setItem("datos", JSON.stringify(datos))
+
+const now = new Date();
+//document.cookie = `datos=${JSON.stringify(datos)};expires=${new Date(now.getTime() + 2 * 60000)}`
